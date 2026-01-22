84e64152d4551d322180ae77113155ba5c107fa6
msoriano15 <mariasorianodelassen@gmail.com>
2026-01-22 08:57:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from streamlit_extras.let_it_rain import rain
 
 # --- CONFIGURATION ---
 # Change this to your actual office address!
-OFFICE_ADDRESS = "ÅSÖGATAN 115, 116 24 STOCKHOLM, SWEDEN" 
+OFFICE_ADDRESS = "ÅSÖGATAN 115,116 24 STOCKHOLM, SWEDEN " 
 
 st.set_page_config(page_title="Team Lunch Roulette", page_icon="🍕", layout="centered")
 
@@ -90,3 +90,33 @@ if lat:
             # Visual flair
             rain(emoji="🥗", font_size=54, falling_speed=4, animation_length="short")
         else:
+            st.warning("No places found! Try increasing the distance in the sidebar.")
+
+    st.divider()
+
+    # 3. DISPLAY THE RESULT (Persistent)
+    if st.session_state.winner_info:
+        res = st.session_state.winner_info
+        
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.success(f"### We're going to:\n**{res['name']}**")
+        with col2:
+            st.info(f"🍴 **Style**\n{res['cuisine']}")
+
+        # Interactive Map - using a static key prevents the 'flicker'
+        m = folium.Map(location=[res['lat'], res['lon']], zoom_start=17)
+        folium.Marker([res['lat'], res['lon']], popup=res['name'], tooltip=res['name'], 
+                      icon=folium.Icon(color='red', icon='info-sign')).add_to(m)
+        folium.Marker([lat, lon], popup="Office", icon=folium.Icon(color='blue', icon='briefcase')).add_to(m)
+        
+        st_folium(m, width=700, height=300, key="lunch_map_static")
+        
+        st.markdown(f"### [↗️ Open in Google Maps](https://www.google.com/maps/dir/?api=1&origin={lat},{lon}&destination={res['lat']},{res['lon']}&travelmode=walking)")
+    else:
+        st.info("Click the button above to decide your fate!")
+
+else:
+    st.error("Could not find office coordinates. Please check the address in the code.")
+
+st.caption("Data provided by OpenStreetMap. If a place is missing, it needs to be added to OSM!")
